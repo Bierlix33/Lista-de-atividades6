f9ecd108c0978879700b165cfcb95eec1d62c876
Bierlix33 <Allangab21@gmail.com>
2025-06-20 14:08:56 -0300

    Update and rename expoente.c to potencia.c

--- potencia.c
+++ potencia.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>//declaração de Bibliotecas 
 int potencia(int base, int expoente){
     int resultado = 1;
-    for(int i = 0; i < expoente; i++){
+    for(int i = 0; i < expoente; i++){ // laço de repetição utilizado para fazer o calculo da potência
         resultado *= base;
 }
 return resultado;
@@ -10,7 +10,7 @@ int main(){
     int base, expoente;
     printf("digite a base e o expoente\n");
     scanf("%d %d", &base, &expoente);
-    int resultado= potencia(base, expoente);
+    int resultado= potencia(base, expoente);//chama a função potencia
     printf("%d elevado a %d é igual a %d\n", base, expoente, resultado);
     return 0;
 }
